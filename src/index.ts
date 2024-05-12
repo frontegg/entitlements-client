cec9afefc40c66d18d7b282bbd9331411f41a20b
Royi Freifeld <royi@frontegg.com>
2024-05-12 13:30:51 +0300

    fix(index.ts): fixed wrong export of files and dirs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+export * from './logging';
+export * from './opa-queries';
 export * from './types';
-export * from './entitlements-client';
 export * from './client-configuration';
+export * from './entitlements.client';
+export * from './entitlements-client-factory';
