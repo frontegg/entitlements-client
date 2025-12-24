d0919faf553105d2649c97acc0b33ff109a12e9f
Niv <niv.rimon@frontegg.com>
2025-12-24 16:01:15 +0200

    added as const

--- src/spicedb/lookup.constants.ts
+++ src/spicedb/lookup.constants.ts
@@ -6,4 +6,4 @@ export const DEFAULT_LOOKUP_LIMIT = 50;
 export const permissionshipMap = new Map<v1.LookupPermissionship, Permissionship>([
 	[v1.LookupPermissionship.HAS_PERMISSION, 'HAS_PERMISSION'],
 	[v1.LookupPermissionship.CONDITIONAL_PERMISSION, 'CONDITIONAL_PERMISSION']
-]);
+] as const);
